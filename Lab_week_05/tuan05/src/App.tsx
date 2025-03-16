8cc03105eb3bcc0e65d3ad985890e50d78cc20c5
H82M19\Student <Student>
2025-03-16 14:24:28 +0700

    tuam05

---
+++ Lab_week_05/tuan05/src/App.tsx
@@ -0,0 +1,45 @@
+import { useReducer } from 'react'
+import './index.css'
+
+function App() {
+  
+  function reducer1(state: any, action: any) {
+    switch (action.type) {
+      case '+':
+        return { ...state, count: state.count + 1 };
+      case '-':
+        return { ...state, count: state.count - 1 };
+      default:
+        return state;
+    }
+  }
+
+  var [result, dispatch] = useReducer(reducer1, { count: 0 });
+//reducer2 cho phan cong tru nhan chia 2 so nhap tu input
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    return e.target.value;
+  }
+
+  return (
+    <>
+      <h1>Tang giam 1 so</h1>
+      <div className='w-screen'>
+        <p className='text-green-500 text-3xl'>{result.count}</p>
+        <button onClick={() => dispatch({ type: '+'})}>+</button>
+        <button onClick={() => dispatch({ type: '-'})}>-</button>
+      </div>
+      <h1>Cong tru nhan chia 2 so</h1> <br />
+      <div>
+        <span>Nhap so a: </span>
+        <input onChange={handleChange} type="number" className='bg-gray-800 border-gray-600 rounded-3xl'/> <br /> <br />
+        <span>Nhap so b: </span>
+        <input onChange={handleChange} type="number"  className='bg-gray-800 border-gray-600 rounded-3xl'/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
