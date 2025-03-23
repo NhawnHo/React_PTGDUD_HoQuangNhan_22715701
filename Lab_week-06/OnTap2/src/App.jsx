19574d70a677a4f3b14cdc201d6893428cc31bcb
H82M19\Student <Student>
2025-03-23 14:26:32 +0700

    Thuc hanh tuan 06

---
+++ Lab_week-06/OnTap2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Header from './component/Header'
+import Footer from './component/Footer'
+import Product from "./component/Product";
+function App() {
+  
+  
+
+  return (
+    <>
+      <Header/>
+      <Product/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
