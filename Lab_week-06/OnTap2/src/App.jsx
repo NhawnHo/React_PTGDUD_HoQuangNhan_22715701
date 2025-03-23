309c524ba65de7937d31e581336dbe66e3a1db07
H82M19\Student <Student>
2025-03-23 14:37:43 +0700

    tuan 06

--- Lab_week-06/OnTap2/src/App.jsx
+++ Lab_week-06/OnTap2/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from './component/Header'
 import Footer from './component/Footer'
-import Product from "./component/Product";
+import Product from "./component/MenuList";
 function App() {
   
   
