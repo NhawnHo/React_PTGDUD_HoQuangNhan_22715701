c588f3a4ab02e28305cde418218c0b0a41684ed9
Hồ Quang Nhân <22715701.nhan@student.iuh.edu.vn>
2025-04-12 00:39:42 +0700

    Commit 4: Get API Data Table

--- Lab_week_07/src/App.jsx
+++ Lab_week_07/src/App.jsx
@@ -103,7 +103,75 @@ function App() {
                                     </tr>
                                 </thead>
                                 <tbody className="divide-y divide-gray-200">
-                                    
+                                    {array.length > 0 ? (
+                                        array.map((item) => (
+                                            <tr
+                                                key={item.id}
+                                                className="hover:bg-gray-50"
+                                            >
+                                                <td className="p-3">
+                                                    <input
+                                                        type="checkbox"
+                                                        className="w-4 h-4 accent-pink-500 cursor-pointer"
+                                                    />
+                                                </td>
+                                                <td className="p-3 w-[250px]">
+                                                    <div className="flex items-center">
+                                                        <img
+                                                            src={item.avatar}
+                                                            alt=""
+                                                            className="w-8 h-8 rounded-full"
+                                                        />
+                                                        <p className="ml-2 font-bold">
+                                                            {item.name}
+                                                        </p>
+                                                    </div>
+                                                </td>
+                                                <td className="p-3">
+                                                    {item.company}
+                                                </td>
+                                                <td className="p-3">
+                                                    ${item.orderValue}
+                                                </td>
+                                                <td className="p-3">
+                                                    {item.orderDate}
+                                                </td>
+                                                <td className="p-3 text-center">
+                                                    <span
+                                                        className={`px-2 py-1 rounded-2xl text-xs ${
+                                                            item.status ===
+                                                            'New'
+                                                                ? 'bg-blue-100 text-blue-700'
+                                                                : item.status ===
+                                                                  'In-progress'
+                                                                ? 'bg-yellow-100 text-yellow-700'
+                                                                : 'bg-green-100 text-green-700'
+                                                        }`}
+                                                    >
+                                                        {item.status}
+                                                    </span>
+                                                </td>
+                                                <td className="p-3 text-right">
+                                                    <button className="text-pink-500 hover:underline">
+                                                        <img
+                                                            src="https://res.cloudinary.com/duongofji/image/upload/v1744188614/create_qj44ru.png"
+                                                            alt="Action"
+                                                            className="h-5 inline"
+                                                        />
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    ) : (
+                                        <tr>
+                                            <td
+                                                colSpan="7"
+                                                className="text-center p-5"
+                                            >
+                                                No data available
+                                            </td>
+                                        </tr>
+                                    )}
                                 </tbody>
                             </table>
                         </div>
