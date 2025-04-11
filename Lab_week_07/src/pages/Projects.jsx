2fc11c1746238a593a8e0aa96c978381be3b24ee
Hồ Quang Nhân <22715701.nhan@student.iuh.edu.vn>
2025-04-11 23:42:52 +0700

    Tuan07

--- Lab_week_07/src/pages/Projects.jsx
+++
@@ -1,6 +0,0 @@
-function Projects() {
-    return (
-        <h2 className="text-2xl font-bold mt-10 text-center">Projects Page</h2>
-    );
-}
-export default Projects;
--- Lab_week_07/src/pages/Teams.jsx
+++
@@ -1,4 +0,0 @@
-function Teams() {
-    return <h2 className="text-2xl font-bold mt-10 text-center">Teams Page</h2>;
-}
-export default Teams;
